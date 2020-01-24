fa28598bac9447185ff5db5729d254f4b649ebba
Kotapati <KOTAPHA1@novartis.net>
2020-01-24 13:31:11 -0500

    vm info test file

---
+++ vminfo.ps1
@@ -0,0 +1,5 @@
+﻿
+
+Get-VM $vmname | Select Name,@{N="IP Address";E={@($_.guest.IPAddress[0])}}, @{N="Cluster";E={Get-Cluster -VM $_}}, `
+@{N="ESX Host";E={Get-VMHost -VM $_}}, `
+@{N="Datastore";E={Get-Datastore -VM $_}} | Ft -AutoSize | Sort-Object -Descending cluster
