98f83783216e3246185816902f9e0e11bd9280d9
Kotapati <KOTAPHA1@novartis.net>
2020-01-24 14:10:07 -0500

    rolesremove

---
+++ removeperm_roles.ps1
@@ -0,0 +1,14 @@
+﻿Set-PowerCLIConfiguration -Scope Session -WebOperationTimeoutSeconds -1 -Confirm:$false
+Set-PowerCLIConfiguration -InvalidCertificateAction ignore -confirm:$false
+
+$vcname = Read-Host" Enter vCenter FQDN name"
+Connect-VIServer $vcname -User.
+
+#remove Permissions
+foreach($role in Get-VIRole *PCS*)
+{
+Get-VIPermission | Where {$_.Role –eq “$role”} | Remove-VIPermission -Confirm:$false
+}
+
+#Remove NVS Roles
+Get-VIRole *PCS* | Remove-VIRole -Confirm:$false
